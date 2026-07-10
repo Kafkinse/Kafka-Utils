9261ecf259d3bae080b8cbcc829dd78372f0a004
Kafkinse <beskorovajnyjkirill844@gmail.com>
2026-07-10 14:50:56 +0300

    Add mod source files

---
+++ main/java/dev/kafka/kafkautils/mixin/ClientBossBarHudAccessor.java
@@ -0,0 +1,14 @@
+package dev.kafka.kafkautils.mixin;
+
+import java.util.Map;
+import java.util.UUID;
+import net.minecraft.class_337;
+import net.minecraft.class_345;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+@Mixin({class_337.class})
+public interface ClientBossBarHudAccessor {
+   @Accessor("field_2060")
+   Map<UUID, class_345> getBossBars();
+}
